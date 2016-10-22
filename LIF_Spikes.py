d3f5c0a8ab2f3b759dd13e4692ddaf57d3542e68
anika-mukherji <anika.mukherji@duke.edu>
2016-10-22 12:11:10 -0400

    Modified voltages to make simulation more realistic

--- LIF_Spikes.py
+++ LIF_Spikes.py
@@ -10,14 +10,15 @@ times = np.arange(0, T+dt, dt)      # time step array
 inputCurrentStart = 0
 inputCurrentEnd = 40
 inputCurrentLen = (inputCurrentEnd - inputCurrentStart) / dt
-current = 3
+current = 50
 inputarray = np.zeros(len(times))
 inputarray[inputCurrentStart:inputCurrentLen] = current
-Vm = np.zeros(len(times))
+Vm = [-60]*(len(times))
+print Vm
 tau_r = 4                           # refractory period
 t_rest = 0                          # initial refractory time
-Vt = 2                              # spike threshold
-V_spike = 0.5                         # spike delta
+Vt = -40                              # spike threshold
+V_spike = 80                         # spike delta
 
 for i, t in enumerate(times):
 
@@ -35,5 +36,5 @@ plt.plot(times, inputarray)
 plt.title("Leaky Integrate and Fire Neuron With Spikes")
 plt.xlabel("Time (ms)")
 plt.ylabel("Membrane Voltage (mV)")
-plt.ylim(0, 6)
+plt.ylim(-70, 60)
 plt.show()
