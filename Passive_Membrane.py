b27ed7b525bae64edc351d091a7ec22fc192342e
anika-mukherji <anika.mukherji@duke.edu>
2016-10-25 09:51:33 -0400

    Plotting range of currents from -1 to 1 nA, in steps of 200 pA

--- Passive_Membrane.py
+++ Passive_Membrane.py
@@ -1,34 +1,42 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-T = 500                             # total simulation time (millisecond)
+T = 150                             # total simulation time (millisecond)
 dt = 0.125                          # time step (millisecond)
-Rm = 100                            # resistance (megaOhm)
+Rm = 30                             # resistance (megaOhm)
 Cm = 1                              # capacitance (microFaraday/cm^2)
 tau_m = Rm * Cm                     # time membrane constant
 times = np.arange(0, T+dt, dt)      # time step array
 inputCurrentStart = 0
-inputCurrentEnd = 300
+inputCurrentEnd = 100
 inputCurrentLen = (inputCurrentEnd - inputCurrentStart) / dt
-current = 1                         # input current (nanoAmpere)
-inputarray = np.zeros(len(times))
-inputarray[inputCurrentStart:inputCurrentLen] = current
+current = 1                         # default input current (nanoAmpere)
 Vm = np.zeros(len(times))           # initializing array of membrane potentials
                                     # in milliVolts
 t_rest = 0                          # refractory period tracker var
 
-for i, t in enumerate(times):
-
-    if t > t_rest:
-        I = current if (t < inputCurrentEnd and t > inputCurrentStart) else 0
-        Vm[i] = Vm[i-1] + (dt/tau_m)*((-1*Vm[i-1]) + I*Rm)
-
-
 plt.figure(1)
-plt.plot(times, Vm)
-plt.plot(times, inputarray)
+# plt.plot(times, inputarray, "#ffff00")
 plt.title("Passive Membrane Model")
 plt.xlabel("Time (ms)")
 plt.ylabel("Membrane Voltage (mV)")
-plt.ylim(0, 110)
+plt.ylim(-35, 35)
+plt.xlim(0, T)
+
+for x in range(-10, 10, 2):         # range of currents -1 to 1 nA with step of
+                                    # 200 pA (divide by 10)
+    current = float(x)/float(10)
+    # inputarray = np.zeros(len(times))
+    # inputarray[inputCurrentStart:inputCurrentLen] = current
+    # print inputarray
+
+    for i, t in enumerate(times):
+
+        if t > t_rest:
+            I = current if (t < inputCurrentEnd and t > inputCurrentStart) else 0
+            Vm[i] = Vm[i-1] + (dt/tau_m)*((-1*Vm[i-1]) + I*Rm)
+
+    plt.plot(times, Vm, "#ff0066")
+
 plt.show()
+
